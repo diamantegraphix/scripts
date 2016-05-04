8dd93f4edab671d8bb1d998ddbf12086f2b74b9d
Bethany Gresser <bethany@diamantegraphix.com>
2016-05-04 10:20:16 -0400

    added error logging

--- backups/backups.sh
+++ backups/backups.sh
@@ -2,111 +2,156 @@
 # site on same server
 # backup saved on same server
 
-# File listing sites to backup
-# format - [site],[full path to parent folder of site]
-sites="sites.txt"
-
-server_root="/home1/diamanv1"
-
-# Full path to directory where backups should be saved
-backups_dir="$server_root/backups"
-
-# Full path and name of log file
-logfile="$backups_dir/backup_log.txt"
-
-
-now="$(date +'%d_%m_%Y_%H_%M_%S')"
 today="$(date +'_%Y-%m-%d')"
 
+# File structure root
+server_home="/var/www/html"
+
+# Web service root
+web_root="/var/www/html/public_html"
+
+# Backups directory, relative to server home
+backups_dir="backups"
+
+# Full path and name of log file
+logfile="$server_home/$backups_dir/backup.log"
+
+# Full path of file with list of sites to backup; format = [site],[full path to parent folder of site]
+sites="$server_home/$backups_dir/sites.txt"
+
+# Create temporary log file
+tmplog="$server_home/tmplog$(date +'%d%m%Y%H%M%S').log"
+touch $tmplog
+
+# Get error messages from all commands in a pipeline
+set -o pipefail
+
+echo "### Backups started at $(date +'%Y-%m-%d %H:%M:%S') ###" ###>> "$tmplog"
+
 # If backups directorys does not exist, create it
-if [ ! -d "$backups_dir" ]; then
-  mkdir $backups_dir
-  if [ $? -ne 0 ] ; then
-    echo "Error: could not create backups directory"
-    exit 1
+if [ ! -d "$server_home/$backups_dir" ]; then
+  mkdir "$server_home/$backups_dir" 2>>"$tmplog"
+  if [ $? == 0 ] ; then
+    echo "Backups directory $backups_dir/ created" ###>> "$tmplog"
+  else
+    echo "Error: could not create backups directory" ###>> "$tmplog"
+    echo "Backups failed at $(date +'%d/%m/%Y %H:%M:%S') " ###>> "$tmplog"
+    ###cat $tmplog | mail -s "Backups failed" "bethany@diamantegraphix.com"
+    exit 0
   fi
 fi
 
 # If logfile does not exist, create it
 if [ ! -f "$logfile" ]; then
-  touch $logfile
+  touch "$logfile" 2>>"$tmplog"
+  if [ $? != 0 ] ; then errors="ERRORS: "; fi
 fi
 
-echo ""
-echo "Backups started at $(date +'%Y-%m-%d %H:%M:%S')" #>> "$logfile"
-
-
+# Get sites name and path (parent of site directory) from file
 while IFS="," read site path; do 
 
-  if [ "${path: -1}" = "/" ]; then
+  # Remove trailing slash from site directory path
+  if [ "${path: -1}" = "/" ]; then 
     path="${path%?}"
   fi
+  echo "--"
+  echo "# $site" ###>> "$tmplog" 
+  # Check if site directory exists
+  if [ -d "$path/$site" ]; then
+    echo "Filesyetem: $path" ###>> "$tmplog" 
+  else
+    echo "$site backup not completed; $path/$site not found" ###>> "$tmplog"
+    echo "--"
+    errors="ERRORS: "
+    continue
+  fi
 
-  echo "Site: $site" #>> "$logfile"
-
-  # If folder for site in backups directory does not exist, create it
-  if [ ! -d "$backups_dir/$site" ]; then
-    mkdir "$backups_dir/$site"
+  # If backups folder for site does not exist, create it
+  if [ ! -d "$server_home/$backups_dir/$site" ]; then
+    mkdir "$server_home/$backups_dir/$site" 2>>"$tmplog"
+    if [ $? == 0 ]; then 
+      echo "$server_home/$backups_dir/$site directory created" ###>> "$tmplog"
+    else
+      errors="ERRORS: "
+      continue
+    fi    
   fi
 
   # Create archive file and save in backups directory
   zipfile="$backups_dir/$site/$site$today.tar.gz"
-  pushd $path
-  ###tar -zcf "$zipfile" $site
-  popd
-  echo "Backup of directory $path/$site saved as $zipfile" #>> "$logfile"
+  pushd "$path" 2>&1>/dev/null ###2>>"$tmplog"
+  ###tar -zcf "$server_home/$zipfile" $site 2>>"$tmplog"
+  touch "$server_home/$zipfile"
+  if [ $? == 0 ]; then
+    echo "Filesystem backup created: $zipfile" ###>> "$tmplog"    
+  else
+    echo "Filesystem backup: ERROR" ###>> "$tmplog"   
+    errors="ERRORS: "
+    continue
+  fi
+  popd 2>&1>/dev/null ###2>>"$tmplog"
 
   # delete all but 2 most recent file backups
-  echo $(find "$backups_dir/$site" -maxdepth 1 -type f -name "$site*.tar.gz")
-
-  #ls -tp $backups_dir/$site/$site*.tar.gz | tail -n +3 | xargs -I {} rm {}
+  #ls -tp $server_home/$backups_dir/$site/$site*.tar.gz 2>>"$tmplog" | tail -n +3 | xargs -I {} rm {} 2>>"$tmplog"
+  find $server_home/$backups_dir/$site -maxdepth 1 -name "$site*\.tar\.gz" | tail -n +3 | xargs -I {} rm {} 2>>"$tmplog"
 
   # Check if site is wordpress site
   if [ -f "$path/$site/index.php" ]; then
-    check=$(grep "/wp-blog-header.php" $path/$site/index.php | cut -d "'" -f2)
+    check=$(grep "/wp-blog-header.php" "$path/$site/index.php" | cut -d "'" -f2)
 
     if [ -z "$check" ]; then
       # Not a WordPress site
-      wp=""
+      continue
     elif [ "$check" == '/wp-blog-header.php' ]; then
       # Active WordPress install is in site directory
-      wp="/"
+      wp=""
     else
-      # Active WordPress install is in subdirectory of site
+      # Active WordPress install is in subdirectory of site directory
       wp=$(cut -d "/" -f2 <<< "$check")
-      wp="/$wp/"
+      wp="$wp/"
     fi
   else
     # Not a WordPress site
-      wp=""
+    continue
   fi
 
-  # If WordPress site, get database info
-  if [ -n "$wp" ]; then
+  # Get database info for WordPress site
+  wpconfig="$path/$site/${wp}wp-config.php"
+  if [ -f "$wpconfig" ]; then
 
-    # Look for wp-config.php file
-    wpconfig="$path/$site${wp}wp-config.php"
-    if [ -f "$wpconfig" ]; then
+    # Get database credentials
+    mysql_name=$(cut -d "'" -f4 <<< $(grep DB_NAME "$wpconfig"))
+    mysql_user=$(cut -d "'" -f4 <<< $(grep DB_USER "$wpconfig"))
+    mysql_pass=$(cut -d "'" -f4 <<< $(grep DB_PASSWORD "$wpconfig"))
+    #mysql_host=$(cut -d "'" -f4 <<< $(grep DB_HOST "$wpconfig"))
 
-      # Get database credentials
-      mysql_name=$(cut -d "'" -f4 <<< $(grep DB_NAME "$wpconfig"))
-      mysql_user=$(cut -d "'" -f4 <<< $(grep DB_USER "$wpconfig"))
-      mysql_pass=$(cut -d "'" -f4 <<< $(grep DB_PASSWORD "$wpconfig"))
-      #mysql_host=$(cut -d "'" -f4 <<< $(grep DB_HOST "$wpconfig"))
+    echo "Database: $mysql_name" ###>> "$tmplog"
 
-      sqlfile="$backups_dir/$site/$site$today.sql.gz"
-      ###mysqldump --user="$mysql_user" --password="$mysql_pass" --default-character-set=utf8 "$mysql_name" | gzip > "$sqlfile"
-      echo "Backup of database $mysql_name saved as $sqlfile" #>> "$logfile"
+    sqlfile="$backups_dir/$site/$site$today.sql.gz"
+    ###mysqldump --user="$mysql_user" --password="$mysql_pass" --default-character-set=utf8 "$mysql_name" 2>>"$tmplog" | gzip > "$server_home/$sqlfile" 2>>"$tmplog"
 
-      # delete all but 2 most recent database backups
-      ls -tp $backups_dir/$site/$site*.sql.gz | tail -n +3 | xargs -I {} rm {}
+    if [ $? == 0 ]; then
+      echo "Database backup created: $sqlfile" ###>> "$tmplog"
+    else
+      echo "Database backup: ERROR" ###>> "$tmplog"
+      errors="ERRORS: "
+      continue
+    fi  
 
-    else 
-      echo "Config file wp-config.php not found"
-    fi
+    # delete all but 2 most recent database backups
+    ls -tp $server_home/$backups_dir/$site/$site*.sql.gz 2>>"$tmplog" | tail -n +3 | xargs -I {} rm {} 2>>"$tmplog"
 
+   
+  else 
+    echo "Config file wp-config.php not found for WordPress site" ###>> "$tmplog"
+    errors="ERRORS: "
   fi
+done < "$sites"
 
-done < $sites
+echo "--"
+echo "# Backups completed at $(date +'%Y-%m-%d %H:%M:%S')" ###>> "$tmplog"
 
-exit 0
+cat "$tmplog" >> "$logfile"
+rm "$tmplog"
+
+echo " " >> $logfile
